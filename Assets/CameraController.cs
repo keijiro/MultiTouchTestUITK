27077afc8c26f342ec7b18ad01e24ac071243648
Keijiro Takahashi <keijiro@gmail.com>
2025-04-08 22:47:50 +0900

    CameraController

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,66 +1,132 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 using Unity.Mathematics;
+using System;
 
 public sealed class CameraController : MonoBehaviour
 {
     [SerializeField] UIDocument _ui = null;
-    [SerializeField] Transform _pivot = null;
-
-    VisualElement _area;
-    (int id, float2 prev) _drag = (-1, 0);
-
-    bool IsDragActive => _drag.id >= 0;
+    [SerializeField] Transform _pivotNode = null;
+    [SerializeField] Transform _distanceNode = null;
 
     void Start()
     {
-        _area = _ui.rootVisualElement;
-        _area.RegisterCallback<PointerDownEvent>(OnPointerDown);
-        _area.RegisterCallback<PointerMoveEvent>(OnPointerMove);
-        _area.RegisterCallback<PointerUpEvent>(OnPointerUp);
+        var drag = new DragDetector();
+        drag.OnDragging += OnDragging;
+        drag.OnScrolling += OnScrolling;
+
+        var area = _ui.rootVisualElement;
+        area.AddManipulator(drag);
+    }
+
+    void OnDragging(float2 delta)
+    {
+        var area = _ui.rootVisualElement;
+        var height = area.resolvedStyle.height;
+
+        delta /= height;
+
+        var rot = (float3)_pivotNode.localEulerAngles;
+        var limit = math.float2(40, 60);
+        rot.xy = (rot.xy + 180) % 360 - 180;
+        rot.xy = math.clamp(rot.xy + delta.yx * 90, -limit, limit);
+        _pivotNode.localEulerAngles = rot;
+    }
+
+    void OnScrolling(float delta)
+    {
+        var area = _ui.rootVisualElement;
+        var height = area.resolvedStyle.height;
+
+        var dist = _distanceNode.localPosition.z;
+        dist = math.clamp(dist + 4 * delta / height, -8, -2);
+        _distanceNode.localPosition = new float3(0, 0, dist);
+    }
+}
+
+public class DragDetector : PointerManipulator
+{
+    public event Action<float2> OnDragging;
+    public event Action<float> OnScrolling;
+    (int id, float2 pos) _p1;
+    (int id, float2 pos) _p2;
+
+    public DragDetector()
+    {
+        _p1.id = _p2.id = -1;
+        activators.Add(new ManipulatorActivationFilter{button = MouseButton.LeftMouse});
+    }
+
+    protected override void RegisterCallbacksOnTarget()
+    {
+        target.RegisterCallback<PointerDownEvent>(OnPointerDown);
+        target.RegisterCallback<PointerMoveEvent>(OnPointerMove);
+        target.RegisterCallback<PointerUpEvent>(OnPointerUp);
+    }
+
+    protected override void UnregisterCallbacksFromTarget()
+    {
+        target.UnregisterCallback<PointerDownEvent>(OnPointerDown);
+        target.UnregisterCallback<PointerMoveEvent>(OnPointerMove);
+        target.UnregisterCallback<PointerUpEvent>(OnPointerUp);
     }
 
     void OnPointerDown(PointerDownEvent e)
     {
-        if (IsDragActive)
-        {
-            e.StopImmediatePropagation();
-            return;
-        }
+        if (!CanStartManipulation(e)) return;
 
-        _drag = (e.pointerId, math.float3(e.localPosition).xy);
-        _area.CapturePointer(_drag.id);
-        e.StopPropagation();
+        var id = e.pointerId;
+        if (_p1.id == id || _p2.id == id) return;
+
+        var pos = math.float3(e.localPosition).xy;
+
+        if (_p1.id < 0)
+        {
+            _p1 = (id, pos);
+            target.CapturePointer(id);
+            e.StopPropagation();
+        }
+        else if (_p2.id < 0)
+        {
+            _p2 = (id, pos);
+            target.CapturePointer(id);
+            e.StopPropagation();
+        }
     }
 
     void OnPointerMove(PointerMoveEvent e)
     {
-        if (!IsDragActive) return;
-        if (!_area.HasPointerCapture(_drag.id)) return;
-
-        var height = _area.resolvedStyle.height;
-
+        var id = e.pointerId;
         var pos = math.float3(e.localPosition).xy;
-        var delta = (pos - _drag.prev) / height;
-        _drag.prev = pos;
+        float2 delta;
 
-        var rot = (float3)_pivot.localEulerAngles;
-        var limit = math.float2(40, 60);
-        rot.xy = (rot.xy + 180) % 360 - 180;
-        rot.xy = math.clamp(rot.xy + delta.yx * 90, -limit, limit);
-        _pivot.localEulerAngles = rot;
+        if (_p1.id == id)
+            (_p1.pos, delta) = (pos, pos - _p1.pos);
+        else if (_p2.id == id)
+            (_p2.pos, delta) = (pos, pos - _p2.pos);
+        else
+            return;
+
+        if (_p1.id < 0 || _p2.id < 0)
+            OnDragging?.Invoke(delta);
+        else
+            OnScrolling?.Invoke(delta.y / 2);
 
         e.StopPropagation();
     }
 
     void OnPointerUp(PointerUpEvent e)
     {
-        if (!IsDragActive) return;
-        if (!_area.HasPointerCapture(_drag.id)) return;
+        var id = e.pointerId;
 
-        _drag.id = -1;
-        _area.ReleaseMouse();
+        if (_p1.id == id)
+            _p1.id = -1;
+        else if (_p2.id == id)
+            _p2.id = -1;
+        else
+            return;
 
+        target.ReleasePointer(id);
         e.StopPropagation();
     }
 }
