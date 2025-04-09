30b73d75acddcf837bd670a330dc25905caa6476
Keijiro Takahashi <keijiro@gmail.com>
2025-04-09 11:36:24 +0900

    parameter tweaks

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,42 +6,64 @@ namespace MultiTouchTest {
 
 public sealed class CameraController : MonoBehaviour
 {
+    #region Scene object references
+
     [SerializeField] UIDocument _ui = null;
     [SerializeField] Camera _camera = null;
     [SerializeField] Transform _pivotNode = null;
     [SerializeField] Transform _distanceNode = null;
+
+    #endregion
+
+    #region Camera controlling parameters
+
+    [SerializeField] float2 _angleLimit = math.float2(60, 180);
+    [SerializeField] float _angleSpeed = 90;
+    [SerializeField] float2 _distanceLimit = math.float2(3, 6);
+    [SerializeField] float _distanceSpeed = 4;
+    [SerializeField] float2 _fovRange = math.float2(20, 45);
+
+    #endregion
+
+    #region TouchDragManipulator callbacks
+
+    void OnDragging(float2 delta)
+    {
+        var r = (float3)_pivotNode.localEulerAngles;
+        r.xy = (r.xy + 180) % 360 - 180; // (0, 360) => (-180, 180)
+        r.xy += delta.yx * _angleSpeed;
+        r.xy = math.clamp(r.xy, -_angleLimit, _angleLimit);
+        _pivotNode.localEulerAngles = r;
+    }
+
+    void OnScrolling(float delta)
+    {
+        var dist = _distanceNode.localPosition.z;
+        dist += _distanceSpeed * delta;
+        dist = math.clamp(dist, -_distanceLimit.y, -_distanceLimit.x);
+        _distanceNode.localPosition = new float3(0, 0, dist);
+    }
+
+    #endregion
+
+    #region MonoBehaviour implementation
 
     void Start()
     {
         var drag = new TouchDragManipulator();
         drag.OnDragging += OnDragging;
         drag.OnScrolling += OnScrolling;
-
-        var area = _ui.rootVisualElement;
-        area.AddManipulator(drag);
-    }
-
-    void OnDragging(float2 delta)
-    {
-        var rot = (float3)_pivotNode.localEulerAngles;
-        var limit = math.float2(40, 60);
-        rot.xy = (rot.xy + 180) % 360 - 180;
-        rot.xy = math.clamp(rot.xy + delta.yx * 90, -limit, limit);
-        _pivotNode.localEulerAngles = rot;
-    }
-
-    void OnScrolling(float delta)
-    {
-        var dist = _distanceNode.localPosition.z;
-        dist = math.clamp(dist + 4 * delta, -5, -2);
-        _distanceNode.localPosition = new float3(0, 0, dist);
+        _ui.rootVisualElement.AddManipulator(drag);
     }
 
     void Update()
     {
-        var dist = _distanceNode.localPosition.z;
-        _camera.fieldOfView = 20 + (-dist - 2);
+        var dist = -_distanceNode.localPosition.z;
+        var ndist = (dist - _distanceLimit.x) / (_distanceLimit.y - _distanceLimit.x);
+        _camera.fieldOfView = math.lerp(_fovRange.x, _fovRange.y, ndist);
     }
+
+    #endregion
 }
 
 } // namespace MultiTouchTest
