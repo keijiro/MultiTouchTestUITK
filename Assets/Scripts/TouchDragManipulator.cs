c88508e839105c43713cfdd80374b4c547eca698
Keijiro Takahashi <keijiro@gmail.com>
2025-04-09 11:11:18 +0900

    Directory structure

--- Assets/Scripts/TouchDragManipulator.cs
+++ Assets/Scripts/TouchDragManipulator.cs
@@ -3,54 +3,16 @@ using UnityEngine.UIElements;
 using Unity.Mathematics;
 using System;
 
-public sealed class CameraController : MonoBehaviour
-{
-    [SerializeField] UIDocument _ui = null;
-    [SerializeField] Camera _camera = null;
-    [SerializeField] Transform _pivotNode = null;
-    [SerializeField] Transform _distanceNode = null;
+namespace MultiTouchTest {
 
-    void Start()
-    {
-        var drag = new DragDetector();
-        drag.OnDragging += OnDragging;
-        drag.OnScrolling += OnScrolling;
-
-        var area = _ui.rootVisualElement;
-        area.AddManipulator(drag);
-    }
-
-    void OnDragging(float2 delta)
-    {
-        var rot = (float3)_pivotNode.localEulerAngles;
-        var limit = math.float2(40, 60);
-        rot.xy = (rot.xy + 180) % 360 - 180;
-        rot.xy = math.clamp(rot.xy + delta.yx * 90, -limit, limit);
-        _pivotNode.localEulerAngles = rot;
-    }
-
-    void OnScrolling(float delta)
-    {
-        var dist = _distanceNode.localPosition.z;
-        dist = math.clamp(dist + 4 * delta, -5, -2);
-        _distanceNode.localPosition = new float3(0, 0, dist);
-    }
-
-    void Update()
-    {
-        var dist = _distanceNode.localPosition.z;
-        _camera.fieldOfView = 20 + (-dist - 2);
-    }
-}
-
-public class DragDetector : PointerManipulator
+public class TouchDragManipulator : PointerManipulator
 {
     public event Action<float2> OnDragging;
     public event Action<float> OnScrolling;
     (int id, float2 pos) _p1;
     (int id, float2 pos) _p2;
 
-    public DragDetector()
+    public TouchDragManipulator()
     {
         _p1.id = _p2.id = -1;
         activators.Add(new ManipulatorActivationFilter{button = MouseButton.LeftMouse});
@@ -140,3 +102,5 @@ public class DragDetector : PointerManipulator
         e.StopPropagation();
     }
 }
+
+} // namespace MultiTouchTest
